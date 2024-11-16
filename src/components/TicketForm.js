23af6bc5ad9f46d776cb929556ad9649fde4c61e
robmosso <robertomosso85@gmail.com>
2024-11-16 16:14:34 +0100

    implemented crud with reducers

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
 
-export default function TicketForm() {
+export default function TicketForm({ dispatch, editingTicket }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("1");
+
+  useEffect(() => {
+    if (editingTicket) {
+      setTitle(editingTicket.title);
+      setDescription(editingTicket.description);
+      setPriority(editingTicket.priority);
+    } else {
+      clearForm();
+    }
+  }, [editingTicket]);
 
   const priorityLables = {
     1: "Low",
@@ -22,13 +32,26 @@ export default function TicketForm() {
     e.preventDefault();
 
     const ticketData = {
-      id: new Date().toISOString(),
+      id: editingTicket ? editingTicket.id : new Date().toISOString(),
       title,
       description,
       priority,
     };
 
+    dispatch({
+      type: editingTicket ? "UPDATE_TICKET" : "ADD_TICKET",
+      payload: ticketData,
+    });
+
     clearForm();
+  };
+
+  const handleCancelEdit = () => {
+    clearForm();
+
+    dispatch({
+      type: "CLEAR_EDITING_TICKET",
+    });
   };
 
   return (
@@ -75,6 +98,12 @@ export default function TicketForm() {
       <button type="submit" className="button">
         Submit
       </button>
+
+      {editingTicket && (
+        <button type="button" className="button" onClick={handleCancelEdit}>
+          Cancel edit
+        </button>
+      )}
     </form>
   );
 }
---
+++ src/components/TicketList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles.css";
+import TicketItem from "./TicketItem";
+
+export default function TicketList({ tickets, dispatch }) {
+  return (
+    <div className="ticket-list">
+      {tickets.map((ticket) => (
+        <TicketItem key={ticket.id} dispatch={dispatch} ticket={ticket} />
+      ))}
+    </div>
+  );
+}
